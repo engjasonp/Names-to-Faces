e77320731a2d59b495cc68676394926dc70fc3ac
engjasonp <engjasonp@gmail.com>
2015-08-14 23:11:08 -0700

    Modified ViewController's class to include UICollectionViewDataSource, UICollectionViewDelegate. Added 2 methods to conform to the UICollectionViewDataSource protocol.

--- Names to Faces/ViewController.swift
+++ Names to Faces/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
@@ -19,6 +19,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+        return cell
     }
 
 

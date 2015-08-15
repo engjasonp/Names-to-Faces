6d6257f1b248271d31dfa2443a157a969f391e3a
engjasonp <engjasonp@gmail.com>
2015-08-14 17:46:47 -0700

    Added UILabel inside of UIImageView. Added new cocoa touch class PersonCell.swift to refer to both the UILabel and UIImageView. Added 4 constraints to collection view to make sure view stays edge to edge within view controller.

---
+++ PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Jason Eng on 8/14/15.
+//  Copyright (c) 2015 EngJason. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}

0aca4c2a2fd15101b773f9568faa053cd505ddc5
engjasonp <engjasonp@gmail.com>
2015-08-16 01:40:20 -0700

    Modified func collectionView to return people.count. Added border specs to each cell and allowed user to edit name of cell.

--- Names to Faces/ViewController.swift
+++ Names to Faces/ViewController.swift
@@ -18,11 +18,24 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+        
+        let person = people[indexPath.item]
+        
+        cell.name.text = person.name
+        
+        let path = getDocumentsDirectory().stringByAppendingPathComponent(person.image)
+        cell.imageView.image = UIImage(contentsOfFile: path)
+        
+        cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor
+        cell.imageView.layer.borderWidth = 2
+        cell.imageView.layer.cornerRadius = 3
+        cell.layer.cornerRadius = 7
+        
         return cell
     }
     
@@ -57,6 +70,24 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         collectionView.reloadData()
         
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        let person = people[indexPath.item]
+        
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .Alert)
+        ac.addTextFieldWithConfigurationHandler(nil)
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        ac.addAction(UIAlertAction(title: "OK", style: .Default) { [unowned self, ac] _ in
+            let newName = ac.textFields![0] as! UITextField
+            person.name = newName.text
+            
+            self.collectionView.reloadData()
+            })
+        
+        presentViewController(ac, animated: true, completion: nil)
     }
     
     func getDocumentsDirectory() -> String {
